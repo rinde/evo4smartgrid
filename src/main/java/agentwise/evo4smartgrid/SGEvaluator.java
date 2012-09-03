a8a6a4dcf1d6cd4bf2152a43f4b1cec40f537fbf
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-09-03 15:14:45 +0200

    adaptations to interface change

--- src/main/java/agentwise/evo4smartgrid/SGEvaluator.java
+++ src/main/java/agentwise/evo4smartgrid/SGEvaluator.java
@@ -12,7 +12,10 @@ import java.util.Collection;
 import rinde.cloud.javainterface.Computer;
 import rinde.evo4mas.evo.gp.GPComputationResultImpl;
 import rinde.evo4mas.evo.gp.GPEvaluator;
+import rinde.evo4mas.evo.gp.GPFunc;
 import rinde.evo4mas.evo.gp.GPProgram;
+import ec.EvolutionState;
+import ec.gp.GPTree;
 
 /**
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
@@ -32,11 +35,6 @@ public class SGEvaluator extends GPEvaluator<SGCompJob, GPComputationResultImpl,
 	}
 
 	@Override
-	protected Collection<SGCompJob> createComputationJobs(GPProgram<SGContext> program) {
-		return asList(new SGCompJob(program, scenario));
-	}
-
-	@Override
 	protected Computer<SGCompJob, GPComputationResultImpl> createComputer() {
 		return new SGSimulator();
 	}
@@ -44,5 +42,10 @@ public class SGEvaluator extends GPEvaluator<SGCompJob, GPComputationResultImpl,
 	@Override
 	protected int expectedNumberOfResultsPerGPIndividual() {
 		return 1;
+	}
+
+	@Override
+	protected Collection<SGCompJob> createComputationJobs(GPTree[] trees, EvolutionState state) {
+		return asList(new SGCompJob(new GPProgram<SGContext>((GPFunc<SGContext>) trees[0].child), scenario));
 	}
 }
